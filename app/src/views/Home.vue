71e05a087a633b9320e09854a721d86168dba0d6
0928ldl <1367249475@qq.com>
2019-12-02 11:39:11 +0800

    first

---
+++ app/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <span @click="fn()">{{list}}</span>
+  </div>
+</template>
+
+<script>
+import {mapState} from "vuex"
+export default {
+  name: 'home',
+  components: {
+   
+  },
+  methods:{
+    fn(){
+      console.log('1')
+    }
+  },
+  computed:{
+    ...mapState(["list"])
+  }
+}
+</script>
